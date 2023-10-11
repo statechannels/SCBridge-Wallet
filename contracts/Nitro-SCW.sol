694141b714b1c3bff2f87ff16ecde4988f1fda49
lalexgap <me@alexgap.ca>
2023-10-11 11:35:57 -0700

    Implement IAccount interface

--- contracts/Nitro-SCW.sol
+++ contracts/Nitro-SCW.sol
@@ -3,4 +3,10 @@ pragma solidity 0.8.17;
 // SPDX-License-Identifier: MIT
 import {NitroAdjudicator} from "@statechannels/nitro-protocol/contracts/NitroAdjudicator.sol";
 
-contract NitroSmartContractWallet is NitroAdjudicator {}
+contract NitroSmartContractWallet is NitroAdjudicator, IAccount {
+    function validateUserOp(
+        UserOperation calldata userOp,
+        bytes32 userOpHash,
+        uint256 missingAccountFunds
+    ) external returns (uint256 validationData) {}
+}
