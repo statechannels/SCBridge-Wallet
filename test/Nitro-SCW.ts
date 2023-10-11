cdfcee93570765be0ace9ab9f92ac7522b9bfd6d
lalexgap <me@alexgap.ca>
2023-10-11 16:28:40 -0700

    add empty call

--- test/Nitro-SCW.ts
+++ test/Nitro-SCW.ts
@@ -1,37 +1,44 @@
-import {
-  loadFixture
-} from '@nomicfoundation/hardhat-toolbox-viem/network-helpers'
-
 import hre from 'hardhat'
+import { type NitroSmartContractWallet } from '../typechain-types'
+
+import { type UserOperationStruct } from '../typechain-types/contracts/Nitro-SCW.sol/NitroSmartContractWallet'
 
 describe('Nitro-SCW', function () {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
-  async function deployNitroSCW (): Promise<any> {
-    // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await hre.viem.getWalletClients()
-
-    const nitroSCW = await hre.viem.deployContract('NitroSmartContractWallet')
-
-    const publicClient = await hre.viem.getPublicClient()
-
-    return {
-      nitroSCW,
-      owner,
-      otherAccount,
-      publicClient
-    }
+  async function deployNitroSCW (): Promise<NitroSmartContractWallet> {
+    const deployer = await hre.ethers.getContractFactory('NitroSmartContractWallet')
+    return deployer.deploy()
   }
 
   describe('Deployment', function () {
     it('Should deploy the nitro SCW', async function () {
-      await loadFixture(deployNitroSCW)
+      await deployNitroSCW()
     })
   })
 
   describe('validateUserOp', function () {
-    it.skip('Should return success if the userOp is signed by the owner and the intermediary', async function () {})
+    it('Should return success if the userOp is signed by the owner and the intermediary', async function () {
+      const nitroSCW = await deployNitroSCW()
+
+      const userOp: UserOperationStruct = {
+        sender: hre.ethers.ZeroAddress,
+        nonce: 0,
+        initCode: hre.ethers.ZeroHash,
+        callData: hre.ethers.ZeroHash,
+        callGasLimit: 0,
+        verificationGasLimit: 0,
+        preVerificationGas: 0,
+        maxFeePerGas: 0,
+        maxPriorityFeePerGas: 0,
+        paymasterAndData: hre.ethers.ZeroHash,
+        signature: hre.ethers.ZeroHash
+      }
+
+      const result = await nitroSCW.validateUserOp(userOp, hre.ethers.ZeroHash, 0)
+      await result.wait()
+    })
     it.skip('Should only allow challenges if the userOp is signed by the owner', async function () {})
   })
 })
