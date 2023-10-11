1049463e1663ed0ee2e8603c4173472ebb00ff22
lalexgap <me@alexgap.ca>
2023-10-11 11:28:18 -0700

    Implement nitro SCW skeleton

---
+++ contracts/Nitro-SCW.sol
@@ -0,0 +1,6 @@
+pragma solidity 0.8.17;
+
+// SPDX-License-Identifier: MIT
+import {NitroAdjudicator} from "@statechannels/nitro-protocol/contracts/NitroAdjudicator.sol";
+
+contract NitroSmartContractWallet is NitroAdjudicator {}
