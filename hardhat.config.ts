0a5f803949be8d1b74dcc97c60b89202b89cdd22
lalexgap <me@alexgap.ca>
2023-10-11 12:10:21 -0700

    enable optimization to make the contract smaller

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,13 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {version: "0.8.17",  settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },},
+  
 };
 
 export default config;
