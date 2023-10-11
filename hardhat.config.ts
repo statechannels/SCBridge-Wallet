898b9e49d374e666e8b19de2769f7bc11ed727dc
lalexgap <me@alexgap.ca>
2023-10-11 16:27:16 -0700

    switch to ethers

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,5 @@
 import { type HardhatUserConfig } from 'hardhat/config'
-import '@nomicfoundation/hardhat-toolbox-viem'
+
 import '@typechain/hardhat'
 import '@nomicfoundation/hardhat-ethers'
 import '@nomicfoundation/hardhat-chai-matchers'
