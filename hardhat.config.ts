9ece1b787ea1e4f52eb193762fb36800aa1b88db
lalexgap <me@alexgap.ca>
2023-10-11 15:24:50 -0700

    add typechain

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,8 @@
 import { type HardhatUserConfig } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox-viem'
-
+import '@typechain/hardhat'
+import '@nomicfoundation/hardhat-ethers'
+import '@nomicfoundation/hardhat-chai-matchers'
 const config: HardhatUserConfig = {
   solidity: {
     version: '0.8.17',
