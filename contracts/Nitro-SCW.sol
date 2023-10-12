3d3a7785526179f356c35d0e671ec4f5aeece00c
lalexgap <me@alexgap.ca>
2023-10-12 08:03:16 -0700

    very basic sig test

--- contracts/Nitro-SCW.sol
+++ contracts/Nitro-SCW.sol
@@ -4,10 +4,12 @@ pragma solidity 0.8.19;
 
 import {IAccount} from "contracts/interfaces/IAccount.sol";
 import {UserOperation} from "contracts/interfaces/UserOperation.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 contract NitroSmartContractWallet is IAccount {
     address public owner;
     address public intermediary;
+    using ECDSA for bytes32;
 
     function validateUserOp(
         UserOperation calldata userOp,
@@ -32,49 +34,13 @@ contract NitroSmartContractWallet is IAccount {
         if (userOp.signature.length == 2 * 65) {
             revert("TODO: Handle UserOp signed by owner and intermediary");
         } else if (userOp.signature.length == 65) {
-            if (owner != recoverSigner(userOpHash, userOp.signature)) {
+            // TODO: Verify the userOp is for a challenge specifically
+            bytes32 hash = userOpHash.toEthSignedMessageHash();
+            if (owner != hash.recover(userOp.signature))
                 return SIG_VALIDATION_FAILED;
-            }
             return 0;
         } else {
             revert("Invalid signature length");
         }
     }
 
-    // TODO: These are copied from https://solidity-by-example.org/signature/
-    // If we upgrade to 0.8.19 of solidity we can use the built in recover
-    function recoverSigner(
-        bytes32 _ethSignedMessageHash,
-        bytes memory _signature
-    ) public pure returns (address) {
-        (bytes32 r, bytes32 s, uint8 v) = splitSignature(_signature);
-
-        return ecrecover(_ethSignedMessageHash, v, r, s);
-    }
-
-    function splitSignature(
-        bytes memory sig
-    ) public pure returns (bytes32 r, bytes32 s, uint8 v) {
-        require(sig.length == 65, "invalid signature length");
-
-        assembly {
-            /*
-            First 32 bytes stores the length of the signature
-
-            add(sig, 32) = pointer of sig + 32
-            effectively, skips first 32 bytes of signature
-
-            mload(p) loads next 32 bytes starting at the memory address p into memory
-            */
-
-            // first 32 bytes, after the length prefix
-            r := mload(add(sig, 32))
-            // second 32 bytes
-            s := mload(add(sig, 64))
-            // final byte (first byte of the next 32 bytes)
-            v := byte(0, mload(add(sig, 96)))
-        }
-
-        // implicitly return (r, s, v)
-    }
-}
--- test/Nitro-SCW.ts
+++ test/Nitro-SCW.ts
@@ -1,18 +1,24 @@
 import hre, { ethers } from 'hardhat'
 import { type NitroSmartContractWallet } from '../typechain-types'
-
+import { type BaseWallet } from 'ethers'
 import { type UserOperationStruct } from '../typechain-types/contracts/Nitro-SCW.sol/NitroSmartContractWallet'
 import { expect } from 'chai'
-import { type Signer } from 'ethers'
+import { getUserOpHash, signUserOp } from './UserOp'
 
 describe('Nitro-SCW', function () {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
-  async function deployNitroSCW (): Promise<{ nitroSCW: NitroSmartContractWallet, owner: Signer, intermediary: Signer }> {
+  async function deployNitroSCW (): Promise<{ nitroSCW: NitroSmartContractWallet, owner: BaseWallet, intermediary: BaseWallet }> {
     const deployer = await hre.ethers.getContractFactory('NitroSmartContractWallet')
-    const owner = (await ethers.getSigners())[0]
-    const intermediary = (await ethers.getSigners())[1]
+
+    const owner = ethers.Wallet.createRandom()
+    const intermediary = ethers.Wallet.createRandom()
+    const hardhatFundedAccount = (await hre.ethers.getSigners())[0]
+
+    hardhatFundedAccount.sendTransaction({ to: owner.address, value: ethers.parseEther('1.0') })
+    hardhatFundedAccount.sendTransaction({ to: intermediary.address, value: ethers.parseEther('1.0') })
+
     const nitroSCW = await deployer.deploy(owner, intermediary)
     return { nitroSCW, owner, intermediary }
   }
@@ -25,10 +31,10 @@ describe('Nitro-SCW', function () {
 
   describe('validateUserOp', function () {
     it('Should return success if the userOp is signed by the owner and the intermediary', async function () {
-      const { nitroSCW } = await deployNitroSCW()
-
+      const { nitroSCW, owner } = await deployNitroSCW()
+      const n = await ethers.provider.getNetwork()
       const userOp: UserOperationStruct = {
-        sender: hre.ethers.ZeroAddress,
+        sender: owner.address,
         nonce: 0,
         initCode: hre.ethers.ZeroHash,
         callData: hre.ethers.ZeroHash,
@@ -40,8 +46,14 @@ describe('Nitro-SCW', function () {
         paymasterAndData: hre.ethers.ZeroHash,
         signature: hre.ethers.ZeroHash
       }
+
+      const ownerSig = signUserOp(userOp, owner, ethers.ZeroAddress, Number(n.chainId))
+      const hash = getUserOpHash(userOp, ethers.ZeroAddress, Number(n.chainId))
+
+      userOp.signature = ownerSig
+      console.log(userOp)
       // staticCall forces an eth_call, allowing us to easily check the result
-      const result = await nitroSCW.getFunction('validateUserOp').staticCall(userOp, hre.ethers.ZeroHash, 0)
+      const result = await nitroSCW.getFunction('validateUserOp').staticCall(userOp, hash, 0)
       expect(result).to.equal(0)
     })
     it.skip('Should only allow challenges if the userOp is signed by the owner', async function () {})
