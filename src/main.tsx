64bd1dd206c0e1fe5b5eec1df292c61fd654272c
George Knee <georgeknee@googlemail.com>
2023-10-16 23:10:00 +0100

    remove strict mode
    this causes everything to be rendered twice on the dev server, which messes things up

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,5 @@ import WalletContainer from "./WalletContainer";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <WalletContainer />
-  </React.StrictMode>,
+  <WalletContainer />,
 );
