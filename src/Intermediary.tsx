6be880b780f091fded3e2a6619d7a000d6f62979
George Knee <georgeknee@googlemail.com>
2023-10-17 14:09:00 +0100

    harmonize intermediary with wallets

--- src/Intermediary.tsx
+++ src/Intermediary.tsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import logo from "./assets/logo.png";
 import { IntermediaryClient } from "../clients/IntermediaryClient";
 import { AddressIcon } from "./AddressIcon";
+import { Card } from "@mui/material";
 export const Intermediary: React.FunctionComponent = () => {
   const [ownerBalance, setOwnerBalance] = useState(0);
   const [intermediaryBalance, setIntermediaryBalance] = useState(0);
@@ -39,14 +40,23 @@ export const Intermediary: React.FunctionComponent = () => {
   }, []);
 
   return (
-    <>
+    <Card
+      variant="outlined"
+      sx={{
+        bgcolor: "background.paper",
+        boxShadow: 1,
+        borderRadius: 2,
+        p: 2,
+        minWidth: 300,
+      }}
+    >
       <img
         src={logo}
         className="logo"
         alt="SCBridge-Wallet Logo"
         style={{ height: "25vh" }}
       />
-      <h1>Intermediary</h1>
+      <h2>Intermediary</h2>
       <AddressIcon
         address={
           // @ts-expect-error
@@ -57,6 +67,6 @@ export const Intermediary: React.FunctionComponent = () => {
         <p>Owner balance: {ownerBalance}</p>
         <p>Intermediary balance: {intermediaryBalance}</p>
       </div>
-    </>
+    </Card>
   );
 };
