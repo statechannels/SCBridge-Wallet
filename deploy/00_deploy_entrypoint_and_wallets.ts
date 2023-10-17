06148eafd863ced526dc58fe914c31011b35a4c9
lalexgap <me@alexgap.ca>
2023-10-17 11:41:02 -0700

    allow configurable salt

--- deploy/00_deploy_entrypoint_and_wallets.ts
+++ deploy/00_deploy_entrypoint_and_wallets.ts
@@ -1,22 +1,30 @@
 import { type HardhatRuntimeEnvironment } from "hardhat/types";
 import { type DeployFunction } from "hardhat-deploy/types";
-import { getNamedAccounts } from "hardhat";
+import { ethers, getNamedAccounts } from "hardhat";
 import dotenv from "dotenv";
-const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  dotenv.config();
 
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  console.log("Starting deployment...");
+  dotenv.config();
   const aliceAddress = process.env.VITE_ALICE_ADDRESS;
   const bobAddress = process.env.VITE_BOB_ADDRESS;
   const ireneAddress = process.env.VITE_IRENE_ADDRESS;
-
+  const salt = ethers.encodeBytes32String(
+    process.env.VITE_DEPLOY_SALT_STRING ?? "",
+  );
+  console.log(
+    `Using salt ${salt} based on salt string '${
+      process.env.VITE_DEPLOY_SALT_STRING ?? ""
+    }'`,
+  );
   const { deployments } = hre;
   const { deterministic } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  console.log("Starting deployment...");
   const entrypoint = await deterministic("EntryPoint", {
     from: deployer,
     args: [],
+    salt,
 
     autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
   });
@@ -26,6 +34,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const aliceWallet = await deterministic("SCBridgeWallet", {
     from: deployer,
     args: [aliceAddress, ireneAddress, entrypoint.address],
+    salt,
 
     autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
   });
@@ -39,6 +48,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const bobWallet = await deterministic("SCBridgeWallet", {
     from: deployer,
     args: [bobAddress, ireneAddress, entrypoint.address],
+    salt,
 
     autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
   });
