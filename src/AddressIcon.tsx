7af8720dd7c768cd8b15f512c9d258bf0e49e823
George Knee <georgeknee@googlemail.com>
2023-10-17 14:01:53 +0100

    make it all look better

--- src/AddressIcon.tsx
+++ src/AddressIcon.tsx
@@ -1,4 +1,4 @@
-import { Tooltip } from "@mui/material";
+import { Avatar, Tooltip } from "@mui/material";
 import { blo } from "blo";
 import React, { type FunctionComponent } from "react";
 
@@ -9,5 +9,17 @@ export const AddressIcon: FunctionComponent<{
     <Tooltip title={props.address}>
       <img alt={props.address} src={blo(props.address)} />
     </Tooltip>
+  );
+};
+
+export const AddressIconSmall: FunctionComponent<{
+  address: `0x${string}`;
+}> = (props: { address: `0x${string}` }, ref) => {
+  return (
+    <Avatar
+      {...props}
+      src={blo(props.address)}
+      sx={{ width: 24, height: 24 }}
+    />
   );
 };
