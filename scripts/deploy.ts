1b79b1009f93dfd838bf9af5a105d7ccbe295daa
lalexgap <me@alexgap.ca>
2023-10-18 14:01:59 -0700

    clean up deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,8 @@
 import { ethers } from "hardhat";
 import dotenv from "dotenv";
 import { EntryPoint__factory } from "../typechain-types";
+import { type HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
+
 const deployFunc = async function (): Promise<void> {
   const hardhatFundedAccount = (await ethers.getSigners())[0];
   const startingBalance = await ethers.provider.getBalance(
@@ -11,14 +13,15 @@ const deployFunc = async function (): Promise<void> {
   );
   console.log("Starting deployment...");
 
+  const aliceAddress = process.env.VITE_ALICE_ADDRESS ?? "";
+  const bobAddress = process.env.VITE_BOB_ADDRESS ?? "";
+  const ireneAddress = process.env.VITE_IRENE_ADDRESS ?? "";
+
   const entryPointDeployer = await ethers.getContractFactory("EntryPoint");
   const entrypoint = await entryPointDeployer.deploy();
   const walletDeployer = await ethers.getContractFactory("SCBridgeWallet");
   console.log("EntryPoint deployed to:", await entrypoint.getAddress());
   dotenv.config();
-  const aliceAddress = process.env.VITE_ALICE_ADDRESS;
-  const bobAddress = process.env.VITE_BOB_ADDRESS;
-  const ireneAddress = process.env.VITE_IRENE_ADDRESS;
 
   const aliceWallet = await walletDeployer.deploy(
     aliceAddress,
@@ -33,14 +36,6 @@ const deployFunc = async function (): Promise<void> {
     )}) SCBridgeWallet deployed to: ${await aliceWallet.getAddress()}`,
   );
 
-  const initialFunding = parseInt(process.env.VITE_SCW_DEPOSIT ?? "", 10);
-
-  console.log("Funding Alice wallet with", initialFunding.toString());
-  await hardhatFundedAccount.sendTransaction({
-    to: await aliceWallet.getAddress(),
-    value: initialFunding,
-  });
-
   const bobWallet = await walletDeployer.deploy(
     bobAddress,
     ireneAddress,
@@ -54,37 +49,51 @@ const deployFunc = async function (): Promise<void> {
     )}) SCBridgeWallet deployed to: ${await bobWallet.getAddress()}`,
   );
 
-  console.log("Funding Bob wallet with", initialFunding.toString());
-  await hardhatFundedAccount.sendTransaction({
-    to: await bobWallet.getAddress(),
-    value: initialFunding,
-  });
-
-  // Use a small amount of the initial funding to fund the entrypoint and Irene since it's just for gas fees
-  const entrypointFunding = initialFunding / 10;
-  console.log(
-    `Funding EntryPoint for Alice's wallet with ${entrypointFunding.toString()}`,
+  const initialFunding = parseInt(process.env.VITE_SCW_DEPOSIT ?? "", 10);
+  await fund(
+    [await aliceWallet.getAddress(), await bobWallet.getAddress()],
+    BigInt(initialFunding),
+    hardhatFundedAccount,
   );
-  await EntryPoint__factory.connect(
+  // Fund irene with a smaller amount to pay for gas
+  await fund([ireneAddress], BigInt(initialFunding / 10), hardhatFundedAccount);
+
+  // Fund the Entrypoint to pay for gas for the SCWs
+  await fundEntryPoint(
+    [await aliceWallet.getAddress(), await bobWallet.getAddress()],
+    BigInt(initialFunding / 10),
     await entrypoint.getAddress(),
     hardhatFundedAccount,
-  ).depositTo(await aliceWallet.getAddress(), { value: entrypointFunding });
-
-  console.log(
-    `Funding EntryPoint for Bob's wallet with ${entrypointFunding.toString()}`,
   );
-  await EntryPoint__factory.connect(
-    await entrypoint.getAddress(),
-    hardhatFundedAccount,
-  ).depositTo(await bobWallet.getAddress(), { value: entrypointFunding });
-
-  console.log("Funding Irene wallet with", entrypointFunding.toString());
-  await hardhatFundedAccount.sendTransaction({
-    to: ireneAddress,
-    value: entrypointFunding,
-  });
-
   console.log("Deployment complete!");
 };
+
+async function fundEntryPoint(
+  addresses: string[],
+  amount: bigint,
+  entryPointAddress: string,
+  fundedAccount: HardhatEthersSigner,
+): Promise<void> {
+  for (const address of addresses) {
+    console.log(`Funding EntryPoint for ${address} with ${amount.toString()}`);
+    await EntryPoint__factory.connect(
+      entryPointAddress,
+      fundedAccount,
+    ).depositTo(address, { value: amount });
+  }
+}
+async function fund(
+  addresses: string[],
+  amount: bigint,
+  fundedAccount: HardhatEthersSigner,
+): Promise<void> {
+  for (const address of addresses) {
+    console.log(`Funding ${address} with ${amount.toString()}`);
+    await fundedAccount.sendTransaction({
+      to: address,
+      value: amount,
+    });
+  }
+}
 
 void deployFunc();
