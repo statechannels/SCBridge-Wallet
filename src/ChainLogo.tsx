0bde6ea96f4dfa411004e464c4d3f818d8509f4e
lalexgap <me@alexgap.ca>
2023-10-21 12:23:18 -0700

    add logo

--- src/ChainLogo.tsx
+++ src/ChainLogo.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ScrollLogo from "./assets/scroll-logo.svg";
 import PolygonLogo from "./assets/polygon-logo.svg";
+import MantleLogo from "./assets/mantle-logo.svg";
+import FilecoinLogo from "./assets/filecoin-logo.svg";
+
 import { type ChainName } from "./chains";
 
 export const ChainLogo: React.FunctionComponent<{
@@ -14,5 +17,9 @@ export const ChainLogo: React.FunctionComponent<{
       return <img src={PolygonLogo} height={40} />;
     case "scroll":
       return <img src={ScrollLogo} height={40} />;
+    case "filecoin":
+      return <img src={FilecoinLogo} height={40} />;
+    case "mantle":
+      return <img src={MantleLogo} height={40} />;
   }
 };
